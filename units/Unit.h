f420c259c768cfbc910f597d1bc4536cc470d4bf
Alexandr Kuznetsov <caiman@codecrafte.rs>
2016-11-12 19:57:32 +0200

    [unit] Create Unit base class

---
+++ units/Unit.h
@@ -0,0 +1,31 @@
+#ifndef UNIT_H
+#define UNIT_H
+
+#include <iostream>
+#include "../states/State.h"
+#include "../exception.h"
+
+class Unit {
+    protected:
+        std::string name;
+        State* state;
+
+    public:
+        Unit(std::string name, std::string title, int hitPoints, int damage);
+        virtual ~Unit();
+
+        const std::string& getName() const;
+        const std::string& getTitle() const;
+        const int getHitPoints() const;
+        const int getHitPointsLimit() const;
+        const int getDamage() const;
+
+        void takeDamage(int dmg);
+        void addHitPoints(int hp);
+
+        virtual void attack(Unit* enemy) = 0;
+        virtual void counterAttack(Unit* enemy);
+};
+
+
+#endif // UNIT_H
